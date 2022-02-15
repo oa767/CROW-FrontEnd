b9db60502dc495448c9c68f0d49faf219d8e43a4
Randy Genere <reg409@nyu.edu>
2022-02-14 19:26:13 -0500

    undid last commit (accidentally deleted Usernames page)

---
+++ src/pages/Usernames/Usernames.jsx
@@ -0,0 +1,38 @@
+import './username.css';
+
+export default function Username() {
+  return (
+    <div className="content">
+      <h3> Choose a username </h3>
+      <button className="username-item">
+	Default Username #1
+      </button>
+      <button className="username-item">
+        Default Username #2
+      </button>
+      <button className="username-item">
+        Default Username #3
+      </button>
+      <button className="username-item">
+        Default Username #4
+      </button>
+      <button className="username-item">
+        Default Username #5
+      </button>
+      <button className="username-item">
+        Default Username #6
+      </button>
+      <button className="username-item">
+        Default Username #7
+      </button>
+      <button className="username-item">
+        Default Username #8
+      </button>      
+      <button className="username-item">
+        Default Username #9
+      </button>
+      <button className="username-item">
+        Default Username #10
+      </button>
+  );
+};
