f63152f79f65492960568bb89225d3abe3462c1c
chinoguerrero <79092132+chinoguerrero@users.noreply.github.com>
2022-02-17 22:39:34 -0500

    Added guidelines for user names

--- src/pages/Usernames/Usernames.jsx
+++ src/pages/Usernames/Usernames.jsx
@@ -2,6 +2,12 @@ import './usernames.css';
 
 export default function Username() {
   return (
+	  {/* Users on the website can select from a set of 10 different usernames 
+	  before entering a room. If a user does not select a name before entering 
+	  a room the website should give them a random one from this set of names. 
+	  The random user name that is selected must be unique from all other users
+	  in the chatroom.
+	  	*/}
     <div className="content">
       <h3> Choose a username </h3>
       <button className="username-item">
