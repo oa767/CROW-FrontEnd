4a18cc234398bdfa417e4bc59c4d4d31adc9e860
Randy Genere <reg409@nyu.edu>
2022-04-12 00:08:22 -0400

    finished implementing dynamic routing for chatrooms

--- src/pages/Chatroom/Chatroom.jsx
+++ src/pages/Chatroom/Chatroom.jsx
@@ -9,75 +9,109 @@ export default function Chatroom() {
   const params = useParams();
   const roomID = params.roomID;
 
-  const [data, setData] = useState(undefined);
-  const [rooms, setRooms] = useState(undefined);
-  const [isLoading, setIsLoading] = useState(true);
+  const username = localStorage.getItem('username');
 
+  const [data, setData] = useState(undefined);
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [isPrivate, setIsPrivate] = useState(JSON.parse(localStorage.getItem('privateRoom')));
+  const [isNewUser, setIsNewUser] = useState(JSON.parse(localStorage.getItem('newUser')));
+  const [isNewRoom, setIsNewRoom] = useState(true);
+
+  const joinRoom = async() => {
+    if (isPrivate && isNewUser && isNewRoom) {
+      await axios.post(`https://crow249.herokuapp.com/rooms/join/${roomID}/${username}`)
+        .then((response) => {
+          console.log(response.data);
+	  setIsNewUser(false);
+	  localStorage.setItem('newUser', false);
+	  setIsNewRoom(false);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    } else if (isPrivate && isNewUser && !isNewRoom) {
+      await axios.post(`https://crow249.herokuapp.com/rooms/join/${roomID}/${username}`)
+        .then((response) => {
+          console.log(response.data);
+	  setIsNewUser(false);
+          localStorage.setItem('newUser', false);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    }
+  }
+  
   const getRoomData = async() => {
     await axios.get('https://crow249.herokuapp.com/rooms/list')
       .then((response) => {
-	setRooms(response.data);
-	console.log(response.data);
+	for (var i = 0; i < response.data.length; ++i) {
+          if (response.data[i]._id.$oid == roomID) {
+            setData(response.data[i]);
+            console.log(response.data[i]);
+            setIsLoading(false);
+          }
+        }
       })
       .catch(error => {
-        console.log(error);
+	console.log(error);
       })
   }
 
   useEffect(async() => {
-    const getRooms = await getRoomData();
-    for (var i = 0; i < rooms.length; ++i) {
-      if (rooms[i]._id.$oid == roomID) {
-        setData(rooms[i]);
-        console.log(rooms[i]);
-        setIsLoading(false);
-        break;
-      }
-    }
-  }, [])
+    await joinRoom()
+      .catch((error) => {
+	console.log(error);
+      })
+    await getRoomData()
+      .catch((error) => {
+        console.log(error);
+      })
+  }, []);
 
   return (
     <>
-    {!isLoading && (
-    <div className="content chatroom">
-      <div className="sideBarContainer">
-        <div className="topBar">
-          <button
-	    className="topBarTitle chatroom"
-	    onClick={() => history.push('/')}
-          >            
-	    Crow
-          </button>
-        </div>
-        <div className="sidebar">
-          <div className="sidebarContent">
-            {data.list_users ? data.list_users.map((user, index) => (
-              <div
-                key={`${user}-${index}`}
-              >
-                <h3> {user} </h3>
+      {!isLoading &&
+        <div className="content chatroom">
+          <div className="sideBarContainer">
+            <div className="topBar">
+              <button
+	        className="topBarTitle chatroom"
+	        onClick={() => history.push('/')}
+              >            
+	        Crow
+              </button>
+            </div>
+            <div className="sidebar">
+              <div className="sidebarContent">
+                {data.list_users ? data.list_users.map((user, index) => (
+                  <div
+                    key={`${user}-${index}`}
+                  >
+                    <h3> {user} </h3>
+                  </div>
+                )) : (
+                  <h3></h3>
+                )}
               </div>
-            )) : (
-              <h3></h3>
-            )}
+            </div>
+          </div>
+          <div className="chatContainer">
+            <div className="topBar chat">
+              <div className="roomName"> {data.room_name} </div>
+              {isPrivate && <h4 style={{color: "white", width: "250px"}}> {data._id.$oid} </h4>}
+            </div>
+            <div className="chatWindow"></div>
+            <div className="chatbar">
+              <input
+	        className="chatInput"
+                placeholder="Type a message"
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="chatContainer">
-        <div className="topBar chat">
-          <div className="roomName"> {data.room_name} </div>
-          <h4 style={{color: "white", marginLeft: "225px"}}> {data._id.$oid} </h4>
-        </div>
-        <div className="chatWindow"></div>
-        <div className="chatbar">
-          <input
-	    className="chatInput"
-            placeholder="Type a message"
-          />
-        </div>
-      </div>
-    </div>
-    )}
+      }
     </>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,55 +14,51 @@ export default function Home(){
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isCodeModalOpen, setIsCodeModalOpen] = useState(false);
   const [roomName, setRoomName] = useState('');
-  const [username, setUsername] = useState('');
   const [roomCode, setRoomCode] = useState('');
+  const [username, setUsername] = useState('');
+
+  const path = '/chatroom/';
 
   const handleCreateRoomUser = async() => {
     await axios.post(`https://crow249.herokuapp.com/rooms/create/${roomName}`)
-      .then(() => {
-        localStorage.setItem('roomName', roomName);
+      .then((response) => {
+	console.log(response.data);
       })
       .catch(error => {
         console.log(error);
       })
     axios.post(`https://crow249.herokuapp.com/users/create/${username}`)
-      .then(() => {
-        localStorage.setItem('username', username);
-        setIsModalOpen(false);
-        localStorage.setItem('private room', true);
-      })
-      .catch(error => {
-        console.log(error);
-      })
-    axios.get(`https://crow249.herokuapp.com/rooms/${roomName}/id`)
       .then((response) => {
-        localStorage.setItem('roomCode', response.data);
-        setRoomCode(response.data);
-	navigateToPage('/chatroom');
+	console.log(response.data);
+	setIsModalOpen(false);	
       })
       .catch(error => {
         console.log(error);
       })
+    return axios.get(`https://crow249.herokuapp.com/rooms/${roomName}/id`)
   }
 
   const joinPrivateRoom = async() => {
-    const createRoomUser = await handleCreateRoomUser();
-    axios.post(`https://crow249.herokuapp.com/rooms/join/${roomCode}/${username}`)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      })
+    const code = await handleCreateRoomUser()
+			.catch((error) => console.log(error));
+    console.log(code.data);
+    localStorage.setItem('privateRoom', true);
+    localStorage.setItem('newUser', true);
+    localStorage.setItem('username', username);
+    console.log(path.concat(code.data));
+    navigateToPage(path.concat(code.data));
   }
 
   const handleJoinWithCode = () => {
-    axios.post(`https://crow249.herokuapp.com/rooms/join/${roomCode}/${username}`)
+    axios.post(`https://crow249.herokuapp.com/users/create/${username}`)
       .then((response) => {
-        localStorage.setItem('roomCode', roomCode);
-        localStorage.setItem('username', username);
-        localStorage.setItem('private room', true);
-        navigateToPage('/chatroom');
+        console.log(response.data);
+        setIsCodeModalOpen(false);
+	localStorage.setItem('username', username);
+        localStorage.setItem('privateRoom', true);
+        localStorage.setItem('newUser', true);
+        console.log(path.concat(roomCode))
+        navigateToPage(path.concat(roomCode));
       })
       .catch(error => {
         console.log(error);
@@ -128,7 +124,7 @@ export default function Home(){
                   onChange={(e) => setUsername(e.target.value)}
                 />
                 <div className="create-actions">
-                  <button className="button" onClick={handleJoinWithCode}> Create Room </button>
+                  <button className="button" onClick={handleJoinWithCode}> Join Room </button>
                   <button className="button" onClick={() => setIsCodeModalOpen(false)}> Cancel </button>
                 </div>
               </div>
