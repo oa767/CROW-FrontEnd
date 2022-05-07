87867a00e8e3b1c80f9abf08038f0988c800828b
Randy Genere <reg409@nyu.edu>
2022-05-07 00:37:23 -0400

    modified client socket connection

--- Client/src/context/socket.js
+++ Client/src/context/socket.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import {io} from 'socket.io-client';
 
-export const socket = io('http://localhost:8080');
+export const socket = io();
 export const SocketContext = React.createContext();
